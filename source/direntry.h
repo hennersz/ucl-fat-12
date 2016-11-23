728211ff641565b110cc4c32362b0995130be74b
Daniel Gavrilov <dgavrilov95@gmail.com>
2016-11-23 15:00:21 +0000

    Indent with 2 spaces

--- source/direntry.h
+++ source/direntry.h
@@ -1,5 +1,5 @@
 /* $FreeBSD: src/sys/fs/msdosfs/direntry.h,v 1.20 2004/02/19 09:56:58 tjr Exp $ */
-/*	$NetBSD: direntry.h,v 1.14 1997/11/17 15:36:32 ws Exp $	*/
+/*  $NetBSD: direntry.h,v 1.14 1997/11/17 15:36:32 ws Exp $ */
 
 /*-
  * Copyright (C) 1994, 1995, 1997 Wolfgang Solfrank.
@@ -17,7 +17,7 @@
  *    documentation and/or other materials provided with the distribution.
  * 3. All advertising materials mentioning features or use of this software
  *    must display the following acknowledgement:
- *	This product includes software developed by TooLs GmbH.
+ *  This product includes software developed by TooLs GmbH.
  * 4. The name of TooLs GmbH may not be used to endorse or promote products
  *    derived from this software without specific prior written permission.
  *
@@ -56,31 +56,31 @@
  * Structure of a dos directory entry.
  */
 struct direntry {
-	u_int8_t	deName[8];	/* filename, blank filled */
-#define	SLOT_EMPTY	0x00		/* slot has never been used */
-#define	SLOT_E5		0x05		/* the real value is 0xe5 */
-#define	SLOT_DELETED	0xe5		/* file in this slot deleted */
-	u_int8_t	deExtension[3];	/* extension, blank filled */
-	u_int8_t	deAttributes;	/* file attributes */
-#define	ATTR_NORMAL	0x00		/* normal file */
-#define	ATTR_READONLY	0x01		/* file is readonly */
-#define	ATTR_HIDDEN	0x02		/* file is hidden */
-#define	ATTR_SYSTEM	0x04		/* file is a system file */
-#define	ATTR_VOLUME	0x08		/* entry is a volume label */
-#define	ATTR_DIRECTORY	0x10		/* entry is a directory name */
-#define	ATTR_ARCHIVE	0x20		/* file is new or modified */
-	u_int8_t	deLowerCase;	/* NT VFAT lower case flags */
-#define	LCASE_BASE	0x08		/* filename base in lower case */
-#define	LCASE_EXT	0x10		/* filename extension in lower case */
-	u_int8_t	deCHundredth;	/* hundredth of seconds in CTime */
-	u_int8_t	deCTime[2];	/* create time */
-	u_int8_t	deCDate[2];	/* create date */
-	u_int8_t	deADate[2];	/* access date */
-	u_int8_t	deHighClust[2];	/* high bytes of cluster number */
-	u_int8_t	deMTime[2];	/* last update time */
-	u_int8_t	deMDate[2];	/* last update date */
-	u_int8_t	deStartCluster[2]; /* starting cluster of file */
-	u_int8_t	deFileSize[4];	/* size of file in bytes */
+  u_int8_t  deName[8];  /* filename, blank filled */
+#define SLOT_EMPTY  0x00    /* slot has never been used */
+#define SLOT_E5   0x05    /* the real value is 0xe5 */
+#define SLOT_DELETED  0xe5    /* file in this slot deleted */
+  u_int8_t  deExtension[3]; /* extension, blank filled */
+  u_int8_t  deAttributes; /* file attributes */
+#define ATTR_NORMAL 0x00    /* normal file */
+#define ATTR_READONLY 0x01    /* file is readonly */
+#define ATTR_HIDDEN 0x02    /* file is hidden */
+#define ATTR_SYSTEM 0x04    /* file is a system file */
+#define ATTR_VOLUME 0x08    /* entry is a volume label */
+#define ATTR_DIRECTORY  0x10    /* entry is a directory name */
+#define ATTR_ARCHIVE  0x20    /* file is new or modified */
+  u_int8_t  deLowerCase;  /* NT VFAT lower case flags */
+#define LCASE_BASE  0x08    /* filename base in lower case */
+#define LCASE_EXT 0x10    /* filename extension in lower case */
+  u_int8_t  deCHundredth; /* hundredth of seconds in CTime */
+  u_int8_t  deCTime[2]; /* create time */
+  u_int8_t  deCDate[2]; /* create date */
+  u_int8_t  deADate[2]; /* access date */
+  u_int8_t  deHighClust[2]; /* high bytes of cluster number */
+  u_int8_t  deMTime[2]; /* last update time */
+  u_int8_t  deMDate[2]; /* last update date */
+  u_int8_t  deStartCluster[2]; /* starting cluster of file */
+  u_int8_t  deFileSize[4];  /* size of file in bytes */
 };
 
 
@@ -90,21 +90,21 @@ struct direntry {
  * We don't use bitfields because we don't know how compilers for
  * arbitrary machines will lay them out.
  */
-#define DT_2SECONDS_MASK	0x1F	/* seconds divided by 2 */
-#define DT_2SECONDS_SHIFT	0
-#define DT_MINUTES_MASK		0x7E0	/* minutes */
-#define DT_MINUTES_SHIFT	5
-#define DT_HOURS_MASK		0xF800	/* hours */
-#define DT_HOURS_SHIFT		11
+#define DT_2SECONDS_MASK  0x1F  /* seconds divided by 2 */
+#define DT_2SECONDS_SHIFT 0
+#define DT_MINUTES_MASK   0x7E0 /* minutes */
+#define DT_MINUTES_SHIFT  5
+#define DT_HOURS_MASK   0xF800  /* hours */
+#define DT_HOURS_SHIFT    11
 
 /*
  * This is the format of the contents of the deDate field in the direntry
  * structure.
  */
-#define DD_DAY_MASK		0x1F	/* day of month */
-#define DD_DAY_SHIFT		0
-#define DD_MONTH_MASK		0x1E0	/* month */
-#define DD_MONTH_SHIFT		5
-#define DD_YEAR_MASK		0xFE00	/* year - 1980 */
-#define DD_YEAR_SHIFT		9
+#define DD_DAY_MASK   0x1F  /* day of month */
+#define DD_DAY_SHIFT    0
+#define DD_MONTH_MASK   0x1E0 /* month */
+#define DD_MONTH_SHIFT    5
+#define DD_YEAR_MASK    0xFE00  /* year - 1980 */
+#define DD_YEAR_SHIFT   9
 
